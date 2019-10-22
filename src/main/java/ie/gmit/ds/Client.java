1594ed551b82141cedf32e06f8c048711bdb11f4
Cathal <cathalbutler@outlook.com>
2019-10-22 17:24:11 +0100

    Validate functionally added. Code clean up.

--- src/main/java/ie/gmit/ds/Client.java
+++ src/main/java/ie/gmit/ds/Client.java
@@ -1,10 +1,11 @@
 package ie.gmit.ds;
 
-import com.google.protobuf.BoolValue;
+import com.google.protobuf.ByteString;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.StatusRuntimeException;
 
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -16,11 +17,12 @@ import java.util.logging.Logger;
 public class Client {
     // Member Variables
     private final ManagedChannel channel;
-    private final PasswordServiceGrpc.PasswordServiceStub asyncPasswordSerive;
     private final PasswordServiceGrpc.PasswordServiceBlockingStub syncPasswordService;
-    // Member Variables
-    private static final Logger logger =
-            Logger.getLogger(Client.class.getName());
+    private static final Logger logger = Logger.getLogger(Client.class.getName());
+    // Member variables for storing hash & salt
+    private ArrayList<HashedData> hashedDataArrayList;
+    private ByteString hashedPassword;
+    private ByteString salt;
 
     // Constructor
     public Client(String host, int port) {
@@ -30,8 +32,9 @@ public class Client {
                 .usePlaintext()
                 .build();
         // Create a stub & blocking stub with the channel
-        asyncPasswordSerive = PasswordServiceGrpc.newStub(channel);
         syncPasswordService = PasswordServiceGrpc.newBlockingStub(channel);
+        // Array List to store hashed data
+        hashedDataArrayList = new ArrayList<HashedData>();
     }// End Constructor
 
     // Shutdown Method
@@ -40,40 +43,89 @@ public class Client {
     }// End method
 
     // Hash Request
-    public void hashRequest(UserHashRequest newUserRequest) {
+    public void hashRequest(int userId, String password) {
         // Logger
-        logger.info("Client: Making hashing request with details: " + newUserRequest);
-
-        // Instance of response
+        logger.info("\n\nClient: Making hashing request with details: User ID: " + userId + " password: " + password);
+        //Variables
+        HashedData hd = new HashedData();
+        // Creating a request to the Server:
+        UserHashRequest request = UserHashRequest.newBuilder().setUserId(userId).setPassword(password).build();
+        // Instance of User Hash Response
         UserHashResponse response;
         try {
-            // Create the response to the
-            response = syncPasswordService.hash(newUserRequest);
-            //Print response
-            System.out.println(response);
+            // Make a request to the server hash method
+            response = syncPasswordService.hash(request);
+            // Setting object property's
+            //hd.setSalt(response.getSalt());
+            //hd.setHashedPassword(response.getHashedPassword());
+            // Adding data to array list
+            //hashedDataArrayList.add(hd);
+            this.setHashedPassword(response.getHashedPassword());
+            this.setSalt(response.getSalt());
         } catch (StatusRuntimeException ex) {
+            // Log exception if any
             logger.log(Level.WARNING, "RPC failed: {0}", ex.getStatus());
             return;
         }// End try catch
+
+        //Logger for response
+        logger.info("\n\nServer Response:\nUser ID: " + response.getUserId() + "\nHashed Password: "
+                + response.getHashedPassword() + "\nSalt: " + response.getSalt());
     }// End method
 
+    public void validateRequest(String password, ByteString hashedPassword, ByteString salt) {
+        // Logger
+        logger.info("\n\nClient: Making Validation request with details Password: " + password + "\nHashed Password: "
+                + hashedPassword + "\nSalt " + salt);
+        //Creating a request to the server
+        ValidateRequest request = ValidateRequest.newBuilder().setPassword(password).setHashedPassword(hashedPassword)
+                .setSalt(salt)
+                .build();
+        // Instance of Validate response
+        ValidateResponse response;
+        try {
+            // Create the response to the
+            response = syncPasswordService.validate(request);
+        } catch (StatusRuntimeException ex) {
+            // Log exception if any
+            logger.log(Level.WARNING, "RPC failed: {0}", ex.getStatus());
+            return;
+        }// End try catch
+        //Logger for response
+        logger.info("\n\nServer Response: Valid Password (Returns true if the given password and salt match the hashed " +
+                "value, false otherwise)\n:" + response.getValidity());
+    }// End validate request method
+
+    public ByteString getHashedPassword() {
+        return hashedPassword;
+    }
+
+    public void setHashedPassword(ByteString hashedPassword) {
+        this.hashedPassword = hashedPassword;
+    }
+
+    public ByteString getSalt() {
+        return salt;
+    }
+
+    public void setSalt(ByteString salt) {
+        this.salt = salt;
+    }
 
     // Main method
     public static void main(String[] args) throws InterruptedException {
         // Creat an instance of the Client and pass it the host and port:
         Client client = new Client("localhost", 50551);
 
-        // Creating a request to the Server:
-        UserHashRequest requests = UserHashRequest.newBuilder()
-                .setUserId(1234)
-                .setPassword("yurtthecamel")
-                .build();
         try {
             //Send request created above:
-            client.hashRequest(requests);
+            // Hardcoded test data
+            client.hashRequest(1234, "yurtTheCamel");
+            //Run data through validate method
+            client.validateRequest("yurtTheCamel", client.getHashedPassword(), client.getSalt());
         } finally {
             // Don't stop process, keep alive to receive async response
             Thread.currentThread().join();
-        }
+        }//End try catch
     }// End main method
 }// Class
--- src/main/java/ie/gmit/ds/PasswordService.java
+++ src/main/java/ie/gmit/ds/PasswordService.java
@@ -1,63 +1,48 @@
 package ie.gmit.ds;
 
+import com.google.protobuf.ByteString;
 import io.grpc.stub.StreamObserver;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.logging.Logger;
 
 /* Cathal Butler | G00346889 - Distributed Systems Project
  * Password Service Class to handle password.proto Services
  */
 
 public class PasswordService extends PasswordServiceGrpc.PasswordServiceImplBase {
-
-    //Member Variables
-    private static final Logger logger =
-            Logger.getLogger(Client.class.getName());
-    private ArrayList<HashedData> hashedDataArrayList;
-
     // Constructor
     public PasswordService() {
-        hashedDataArrayList = new ArrayList<HashedData>();
     }// End Constructor
 
     @Override
     public void hash(UserHashRequest request, StreamObserver<UserHashResponse> responseObserver) {
         //Variables
-        // New instances of HashedData
-        HashedData hd = new HashedData();
         int userId = request.getUserId();
-
-
-        try {
-            System.out.println("Service: Hash Request " + request);
-            // Generate salt
-            hd.setSalt(Passwords.getNextSalt());
-            // Run Hash method is Passwords class passing it the password in a char array and the salt byte array
-            hd.setHashedPassword(Passwords.hash(request.getPassword().toCharArray(), hd.getSalt()));
-            // Adding data to array list
-            hashedDataArrayList.add(hd);
-            //Console log
-            System.out.println("Added to array list: Salt= " + Arrays.toString(hd.getSalt()) + "\nHashed Password= "+ Arrays.toString(hd.getHashedPassword()));
-            // Converting byte[] to Strings to return in the response:
-            String hashedPassword = new String(hd.getHashedPassword());
-            String salt = new String(hd.getSalt());
-            System.out.println("Test data output: " + hashedPassword + salt);
-            // Response
-            responseObserver.onNext(UserHashResponse.newBuilder().setUserId(userId).setHashedPassword(hashedPassword).setSalt(salt).build());
-
-        } catch (RuntimeException ex) {
-            //responseObserver.onNext(BoolValue.newBuilder().setValue(false).build());
-        }// End try catch
-
+        // Generate salt
+        byte[] salt = Passwords.getNextSalt();
+        // Run Hash method is Passwords class passing it the password in a char array and the salt byte array
+        byte[] hashedPassword = Passwords.hash(request.getPassword().toCharArray(), salt);
+        // Response
+        UserHashResponse response = UserHashResponse.newBuilder().setUserId(userId)
+                .setHashedPassword(ByteString.copyFrom(hashedPassword))
+                .setSalt(ByteString.copyFrom(salt)).build();
+        // Send response
+        responseObserver.onNext(response);
+        // Complete response
         responseObserver.onCompleted();
     }// End method
 
     @Override
     public void validate(ValidateRequest request, StreamObserver<ValidateResponse> responseObserver) {
-        super.validate(request, responseObserver);
+        // Variables
+        char[] password = request.getPassword().toCharArray();
+        byte[] salt = request.getSalt().toByteArray();
+        byte[] expectedHash = request.getHashedPassword().toByteArray();
+        // Run checker method in Passwords Class
+        boolean validity = Passwords.isExpectedPassword(password, salt, expectedHash);
+        //Response
+        ValidateResponse response = ValidateResponse.newBuilder().setValidity(validity).build();
+        // Send response
+        responseObserver.onNext(response);
+        // Complete response
+        responseObserver.onCompleted();
     }// End method
-
-
 }// End class
